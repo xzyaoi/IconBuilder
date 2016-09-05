8bbfe917f561af943a5e9c04daf2e692cff3a661
fermi <i@askfermi.me>
2016-09-05 19:39:12 +0800

    start from cli

--- iconbuilder.py
+++ iconbuilder.py
@@ -4,9 +4,10 @@ import os
 import sys
 import time
 import glob
+import click
 from PIL import Image
 
-class Resizer:
+class IconBuilder:
     def __init__(self,picpath):
         self.picpath=picpath
     def pic_walker(self,path):
@@ -15,20 +16,17 @@ class Resizer:
             filepath,filename = os.path.split(files)  # todo: mark
             target.append(filename)
         return target
-
     def pic_resizer(self,picname,n_w,n_h):
         img=Image.open(picname)
         w,h=img.size
         target=img.resize((int(n_w),int(n_h)),Image.ANTIALIAS)
         return target
- 
     def make_folder(self):
         foldername=self.picpath+'/'+'resized'
         print 'new imgs will be saved to '+foldername
         if not os.path.isdir(foldername):
             os.mkdir(foldername)
         return foldername
- 
     def save_pic(self,target,folder,pixel,name):
         img=target
         target.save(folder+'/'+name+'@'+str(pixel)+'.png')
@@ -40,13 +38,19 @@ class Resizer:
             imgfile=self.picpath+'/'+img
             for each in pixels:
                 self.save_pic(self.pic_resizer(imgfile,each,each),self.picpath+'/resized',each,'logo')
-
+@click.command()
 def main():
     cwd=os.getcwd()
     print cwd
-    resizer=Resizer(cwd)
-    pixels=[32,64,128,256,512,1024]
-    resizer.run_auto(pixels)
+    ib=IconBuilder(cwd)
+    f=open('pixels.txt')
+    line=f.readline()
+    pixels=[]
+    while line:
+        pixels.append(int(line))
+        line=f.readline()
+    f.close()
+    ib.run_auto(pixels)
 
 if __name__ == '__main__':
     sys.exit(int(main() or 0))
--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup,find_packages
 setup(
-    name='ztresizer',
+    name='iconbuilder',
     version='0.0.1',
     keywords=('image','resizer'),
     description='just a simple image resizer',
     license='GPL License',
-    install_requires=['Image'],
+    py_modules=['iconbuilder'],
+    install_requires=['Image','Click'],
     author='Xiaozhe Yaoi@zhitan',
     author_email='xiaozhe.yaoi@gmail.com',
     packages=find_packages(),
     platforms='any',
+    entry_points='''
+        [console_scripts]
+        iconbuilder=iconbuilder:main
+    ''',
 )
